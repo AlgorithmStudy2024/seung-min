39da26304e32177ec816d6769b0014beaf70eb84
esm712 <712esm@hanmail.net>
2024-01-23 15:55:42 +0900

    [#2]Fix : folder naming

--- week_4/BOJ2156/Main.java
+++ week_4/BOJ2156/Main.java
@@ -1,6 +1,7 @@
-package week_4.BOJ1256;
+package week_4.BOJ2156;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
     private static int n;
@@ -11,15 +12,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         init();
         process();
-        System.out.println(dp[n]);
+        System.out.println(dp[n-1]);
     }
     
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        nums = new int[n+1];
-        dp = new int[n+1];
-        for (int i = 1; i <= n; i++) {
+        nums = new int[n];
+        dp = new int[n];
+        for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
             nums[i] = num;
         }
@@ -27,17 +28,22 @@ public class Main {
 
     private static void process() {
         if (n == 1) {
-            dp[1] = nums[1];
+            dp[0] = nums[0];
             return;
         }
         if (n == 2) {
-            dp[1] = nums[1];
-            dp[2] = dp[1] + nums[2];
+            dp[0] = nums[0];
+            dp[1] = dp[0] + nums[1];
             return;
         }
-        dp[1] = nums[1];
-        dp[2] = dp[1] + nums[2];
-        for (int i = 3; i <= n; i++) {
+        dp[0] = nums[0];
+        dp[1] = dp[0] + nums[1];
+        for (int i = 2; i < n; i++) {
+            if (i < 3) {
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+                dp[i] = Math.max(dp[i], nums[i - 1] + nums[i]);
+                continue;
+            }
             dp[i] = Math.max(dp[i - 3] + nums[i - 1] + nums[i], dp[i - 2] + nums[i]);
             dp[i] = Math.max(dp[i - 1], dp[i]);
         }
