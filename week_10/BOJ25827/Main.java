71132c0aae5e05490a2f92227eef1e0d4c11370f
esm712 <712esm@hanmail.net>
2024-03-09 15:47:04 +0900

    [#12]Solve : BOJ25827 100point

--- week_10/BOJ25827/Main.java
+++ week_10/BOJ25827/Main.java
@@ -7,89 +7,64 @@ public class Main {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringBuilder sb = new StringBuilder();
+
+    private static long[] time = new long[24 * 60 * 60 + 1];
+    // time[d] : [d-1,d]에 가지고 있는 값
+    private static long[] sum = new long[24 * 60 * 60 + 1];
+    // sum[d] : [0,1]...[d-1,d]까지의 합
+    // sum[1] : time[1]
+    // sum[2] : sum[1] + time[2]
+    // sum[3] : sum[2] + time[3]
+    // [a,a+1]....[b-1,b] : sum[b] - sum[a]
     
+
     private static int n;
 
-    private static List<Point> arr = new ArrayList<>();
+    private static int k = 1; // 유형 2가 처음 나왔을때를 확인하기 위한 용도
 
-    static class Point {
-        int x;
-        int y;
-
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Point(String startPoint, String endPoint) {
-            StringTokenizer startTime = new StringTokenizer(startPoint, ":");
-            StringTokenizer endTime = new StringTokenizer(endPoint, ":");
-            int h1 = Integer.parseInt(startTime.nextToken()) * 3600;
-            int m1 = Integer.parseInt(startTime.nextToken()) * 60;
-            int s1 = Integer.parseInt(startTime.nextToken());
-
-            int h2 = Integer.parseInt(endTime.nextToken()) * 3600;
-            int m2 = Integer.parseInt(endTime.nextToken()) * 60;
-            int s2 = Integer.parseInt(endTime.nextToken());
-            
-            this.x = h1 + m1 + s1;
-            this.y = h2 + m2 + s2;
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         for (int query = 0; query < n; query++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String num = st.nextToken();
             if (num.equals("1")) {
-                addPoint(st.nextToken(), st.nextToken());
-                continue;
+                String startTime = st.nextToken();
+                String endTime = st.nextToken();
+                int startSec = changeTimeSec(startTime);
+                int endSec = changeTimeSec(endTime);
+                time[startSec+1]++;
+                time[endSec+1]--;
             }
             if (num.equals("2")) {
-                count(st.nextToken(), st.nextToken());
-                continue;
+                String startTime = st.nextToken();
+                String endTime = st.nextToken();
+                int startSec = changeTimeSec(startTime);
+                int endSec = changeTimeSec(endTime);
+                if (k == 1) {
+                    k--;
+                    makeArray();
+                }
+                sb.append(sum[endSec] - sum[startSec]).append("\n");
             }
         }
         System.out.println(sb);
     }
 
-    private static void addPoint(String startPoint, String endPoint) {        
-        arr.add(new Point(startPoint, endPoint));
-    }
-    
-    private static void count(String startPoint, String endPoint) {
-        int ans = 0;
-        Point b = new Point(startPoint, endPoint);
+    private static int changeTimeSec(String time) {
+        StringTokenizer st = new StringTokenizer(time, ":");
+        int hour = 3600 * Integer.parseInt(st.nextToken());
+        int min = 60 * Integer.parseInt(st.nextToken());
+        int sec = Integer.parseInt(st.nextToken());
 
-        for (Point a : arr) {
-            // 1. a가 b보다 앞에 있는 경우, a가 b보다 뒤에 있는 경우
-            if (a.y < b.x || b.y < a.x) {
-                continue;
-            }
-            // 2. a가 b보다 앞에 있고 겹치는 경우
-            if (a.x <= b.x && a.y <= b.y) {
-                ans += a.y - b.x;
-                continue;
-            }
-            // 3. a가 b보다 뒤에 있고 겹치는 경우
-            if (b.x <= a.x && b.y <= a.y) {
-                ans += b.y - a.x;
-                continue;
-            }
-            // 4. a가 b안에 포함되는 경우
-            if (b.x <= a.x && a.y <= b.y) {
-                ans += a.y - a.x;
-                continue;
-            }
-            // 5. b가 a안에 포함되는경우
-            if (a.x <= b.x && b.y <= a.y) {
-                ans += b.y - b.x;
-                continue;
-            }
+        return hour + min + sec;
+    }
+
+    private static void makeArray() {
+        for (int i = 1; i <= 24 * 60 * 60; i++) {
+            time[i] += time[i - 1];
         }
-        sb.append(ans).append("\n");
+        for (int i = 1; i <= 24 * 60 * 60; i++) {
+            sum[i] = sum[i - 1] + time[i];
+        }
     }
-
 }
